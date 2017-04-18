c3beeba38be08cfb26918c2f4c2ca51afa8c2f17
Dan Choi <ygdanchoi@gmail.com>
2017-04-18 14:54:19 -0400

    Implement Root & App components

--- frontend/city_sounds.jsx
+++ frontend/city_sounds.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Root from './components/root';
+import Root from './components/root';
 import configureStore from './store/store';
+
 import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to CitySounds</h1>, root);
+  ReactDOM.render(<Root store={ store } />, root);
 
   window.signup = signup;
   window.login = login;
---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = (props) => (
+  <div>
+    <h1>CitySounds</h1>
+  </div>
+);
+
+export default App;
