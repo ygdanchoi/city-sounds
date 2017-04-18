254b58ccda47468400cbeadd72c50e0c39b28557
Dan Choi <ygdanchoi@gmail.com>
2017-04-18 14:13:37 -0400

    Implement backend auth

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,23 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      login(@user)
+      render 'api/users/show'
+    else
+      render json: { base: ['Invalid username/password'] }, status: 401
+    end
+  end
+
+  def destroy
+    if current_user
+      logout
+      render json: {}
+    else
+      render json: { base: ['Nobody signed in'] }, status: 404
+    end
+  end
+end
--- frontend/city_sounds.jsx
+++ frontend/city_sounds.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import Root from './components/root';
 // import confgureStore from './store/store';
+import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to CitySounds</h1>, root);
+
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
 });
