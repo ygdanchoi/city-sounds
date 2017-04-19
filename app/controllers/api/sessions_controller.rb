bc6ef8df669768dd9139b88445d090e3faabe7d5
Dan Choi <ygdanchoi@gmail.com>
2017-04-18 21:44:46 -0400

    Change invalid credentials message

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -8,7 +8,7 @@ class Api::SessionsController < ApplicationController
       login(@user)
       render 'api/users/show'
     else
-      render json: { base: ['Invalid username/password'] }, status: 401
+      render json: { base: ['Invalid username or password'] }, status: 401
     end
   end
 
