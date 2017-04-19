32b2e5cc040d2725801d24f331bcbb08e70b747a
Dan Choi <ygdanchoi@gmail.com>
2017-04-18 20:00:43 -0400

    Only render GreetingContainer on index page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 
-const App = (props) => (
-  <div>
-    <h1>CitySounds</h1>
-    <GreetingContainer />
-    { props.children }
-  </div>
-);
+const App = (props) => {
+  let greetingContainer = "";
+  if (props.location.pathname === '/') {
+    greetingContainer = <GreetingContainer />;
+  } else {
+    greetingContainer = "";
+  }
+  return (
+    <div>
+      <h1>CitySounds</h1>
+      { greetingContainer }
+      { props.children }
+    </div>
+  );
+};
 
 export default App;
