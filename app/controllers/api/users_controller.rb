a5a76825f5ca3fdfdac2fa8b07333f66fa73b339
Dan Choi <ygdanchoi@gmail.com>
2017-04-20 15:00:02 -0400

    Add create/patch route for users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -22,7 +22,16 @@ class Api::UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render 'api/users/show'
+    else
+      render json: @user.errors, status: 422
+    end
+  end
+
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :avatar, :location, :bio)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#root"
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:index, :show, :create]
+    resources :users, only: [:index, :show, :create, :update]
     resource :session, only: [:create, :destroy]
   end
 end
