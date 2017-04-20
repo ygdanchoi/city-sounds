2dcec7058b045ddbb8f8b37b104fc3030ea722ba
Dan Choi <ygdanchoi@gmail.com>
2017-04-20 14:21:49 -0400

    Add dummy edit links conditionally if on own page

--- frontend/components/user/user_sidebar.jsx
+++ frontend/components/user/user_sidebar.jsx
@@ -4,16 +4,43 @@ import { Link } from 'react-router';
 class UserSidebar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = this.props.user;
+    this.deleteAvatarHandler = this.deleteAvatarHandler.bind(this);
+    this.editLocationHandler = this.editLocationHandler.bind(this);
+    this.editBioHandler = this.editBioHandler.bind(this);
+  }
+
+  deleteAvatarHandler(e) {
+    e.preventDefault();
+  }
+
+  editLocationHandler(e) {
+    e.preventDefault();
+  }
+
+  editBioHandler(e) {
+    e.preventDefault();
   }
 
   render() {
+    const ownProfile = this.props.user.id === this.props.currentUserId;
+    let deleteAvatar = null;
+    let editLocation = null;
+    let editBio = null;
+    if (ownProfile) {
+      deleteAvatar = <a href='' onClick={ this.deleteAvatarHandler }>x</a>;
+      editLocation = <a href='' onClick={ this.editLocationHandler }>edit location</a>;
+      editBio = <a href='' onClick={ this.editBioHandler }>edit bio</a>;
+    }
     return(
       <aside>
         <img src={ this.props.user.avatarUrl } />
+        { deleteAvatar }
         <p>{ this.props.user.username }</p>
         <p>{ this.props.user.location }</p>
+        { editLocation }
         <p>{ this.props.user.bio }</p>
-        <p>{ this.props.user.id === this.props.currentUserId ? 'can edit' : 'cannot edit' }</p>
+        { editBio }
       </aside>
     );
   }
