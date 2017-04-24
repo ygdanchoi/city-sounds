7743a41afdd9b39278cbd7234d93cb289134e56a
Dan Choi <ygdanchoi@gmail.com>
2017-04-24 18:28:26 -0400

    Add title and description to collection form

--- frontend/components/collection_form/collection_form.jsx
+++ frontend/components/collection_form/collection_form.jsx
@@ -4,25 +4,48 @@ import { Link, hashHistory } from 'react-router';
 class CollectionForm extends React.Component {
   constructor(props) {
     super(props);
+    if (this.props.collection) {
+      this.state = {
+        title: this.props.collection.title,
+        description: this.props.collection.description,
+      };
+    } else {
+      this.state = {
+        title: '',
+        description: '',
+      };
+    }
   }
 
   componentDidMount() {
     const id = this.props.collectionId;
-    if (id) {
-      this.props.fetchCollection(this.props.collectionId);
+    if (id && this.props.collection === undefined) {
+      this.props.fetchCollection(id).then(
+        (response) => {
+          this.setState({
+            title: response.collection.title,
+            description: response.collection.description
+          });
+        }
+      );
     }
   }
 
   render() {
-    let collection;
+    let tempHeader;
     if (this.props.collectionId) {
-      collection = this.props.collectionId;
+      tempHeader = `edit collection ${this.props.collectionId}`;
     } else {
-      collection = 'add';
+      tempHeader = 'add collection';
     }
     return (
       <div>
-        <h1>{ collection }</h1>
+        <h1>{ tempHeader }</h1>
+        <p>{ this.state.title === '' ? 'Untitled Collection' : this.state.title }</p>
+        <p>by { this.props.currentUser.username }</p>
+        <input placeholder='collection name' type='text' value= { this.state.title } />
+        <label htmlFor='collection-form-description-input'>about this collection</label>
+        <textarea id='collection-form-desciption-input' value= { this.state.description } />
       </div>
     );
   }
