80cf2cc2ae09968b2968a4a9589d6428774f4372
Dan Choi <ygdanchoi@gmail.com>
2017-04-24 11:56:10 -0400

    Match title-artwork spacing

--- frontend/components/user/collection_list_item.jsx
+++ frontend/components/user/collection_list_item.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const CollectionListItem = (props) => {
   return (
     <li className='collection-list-item'>
-      <figure className='collection-list-item-artwork' >
-        <img src={ props.collection.artworkUrl } />
-      </figure>
-      <p className='collection-list-item-title'>{ props.collection.title }</p>
+      <Link to={ `/collections/${props.collection.id}` }>
+        <figure className='collection-list-item-artwork' >
+          <img src={ props.collection.artworkUrl } />
+        </figure>
+        <p className='collection-list-item-title'>{ props.collection.title }</p>
+      </Link>
     </li>
   );
 };
