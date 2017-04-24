561597a51b6137d2120d4ca8c1adbb29c176b659
Dan Choi <ygdanchoi@gmail.com>
2017-04-24 13:39:47 -0400

    Add props to container

--- frontend/components/collection/collection_container.js
+++ frontend/components/collection/collection_container.js
@@ -1,15 +1,26 @@
 import { connect } from 'react-redux';
 import Collection from './collection';
 
+import { fetchUser, updateUser, updateUserAvatar } from '../../actions/user_actions';
+import { fetchUserCollections } from '../../actions/collection_actions';
+import { fetchCollectionSounds } from '../../actions/sound_actions';
+
 const mapStateToProps = (state, ownProps) => {
   return {
-
+    user: state.users[ownProps.params.userId],
+    currentUser: state.session.currentUser,
+    collections: state.collections,
+    sounds: state.sounds
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-
+    fetchUser: (id) => dispatch(fetchUser(id)),
+    updateUser: (user) => dispatch(updateUser(user)),
+    updateUserAvatar: (id, formData) => dispatch(updateUserAvatar(id, formData)),
+    fetchUserCollections: (userId) => dispatch(fetchUserCollections(userId)),
+    fetchCollectionSounds: (collectionId) => dispatch(fetchCollectionSounds(collectionId)),
   };
 };
 
