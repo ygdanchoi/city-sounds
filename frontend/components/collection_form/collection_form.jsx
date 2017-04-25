0a7f1a31db506b26a96c8188729722e4675c9108
Dan Choi <ygdanchoi@gmail.com>
2017-04-25 09:11:55 -0400

    Consolidate CollectionForm for sanity

--- frontend/components/collection_form/collection_form.jsx
+++ frontend/components/collection_form/collection_form.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link, hashHistory } from 'react-router';
-import SoundForm from './sound_form';
 
 class CollectionForm extends React.Component {
   constructor(props) {
@@ -8,8 +7,7 @@ class CollectionForm extends React.Component {
     this.state = {
       artworkFile: null,
       artworkUrl: '/avatars/original/missing.png',
-      audioFiles: [],
-      audioUrls: [],
+      soundIds: [],
       title: '',
       description: '',
     };
@@ -17,26 +15,18 @@ class CollectionForm extends React.Component {
 
   componentDidMount() {
     const id = this.props.collectionId;
-    if (id && this.props.collection === undefined) {
+    if (id) {
       this.props.fetchCollection(id).then(
         (response) => {
           this.setState({
             artworkUrl: response.collection.artworkUrl,
             title: response.collection.title,
-            description: response.collection.description
-          });
-        }
-      )
-      this.props.fetchCollectionSounds(id).then(
-        (response) => {
-          this.setState({
-            audioUrls: Object.keys(response.sounds).map(
-              id => response.sounds[id].audioUrl
-            ),
-            audioFiles: Array(Object.keys(response.sounds).length)
+            description: response.collection.description,
+            soundIds: response.collection.soundIds,
           });
         }
       );
+      this.props.fetchCollectionSounds(id);
     }
   }
 
@@ -54,13 +44,22 @@ class CollectionForm extends React.Component {
     } else {
       artwork = <img style={ { width: '72px', height: '72px' } } src={ this.state.artworkUrl } />;
     }
+    const sounds = this.state.soundIds.map(
+      (id) => <li key={ id }><p>{ id }</p></li>
+    );
     return (
       <div>
         <h1>{ tempHeader }</h1>
         { artwork }
         <p>{ this.state.title === '' ? 'Untitled Collection' : this.state.title }</p>
         <p>by { this.props.currentUser.username }</p>
-        <SoundForm audioUrls={ this.state.audioUrls } audioFiles={ this.state.audioFiles } />
+          <div>
+            <p>sounds</p>
+            <ul>
+              { sounds }
+            </ul>
+            <a>add sound</a>
+          </div>
         <input placeholder='collection name' type='text' value= { this.state.title } />
         <label htmlFor='collection-form-description-input'>about this collection</label>
         <textarea id='collection-form-desciption-input' value= { this.state.description } />
