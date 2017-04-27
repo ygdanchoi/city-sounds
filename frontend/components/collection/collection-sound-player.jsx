e6d532b933a30b13aa1d7f12aba2310ea7800e0f
Dan Choi <ygdanchoi@gmail.com>
2017-04-27 10:20:11 -0400

    Get currentTime and duration data

--- frontend/components/collection/collection-sound-player.jsx
+++ frontend/components/collection/collection-sound-player.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 class CollectionSoundPlayer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      audioCurrentTime: 0,
+      audioDuration: 0
+    };
   }
 
   componentDidMount() {
@@ -12,6 +16,16 @@ class CollectionSoundPlayer extends React.Component {
       collectionPlayButton.classList.remove('collection-playing');
       collectionPlayButton.classList.add('collection-paused');
     });
+    soundAudio.addEventListener('loadeddata', (() => {
+      this.setState({
+        audioDuration: soundAudio.duration
+      });
+    }).bind(this));
+    setInterval((() => {
+      this.setState({
+        audioCurrentTime: soundAudio.currentTime
+      });
+    }).bind(this), 1000);
   }
 
   playAudio() {
@@ -28,15 +42,20 @@ class CollectionSoundPlayer extends React.Component {
   }
 
   render() {
+    const soundAudio = (
+      <audio id='sound-audio' key={ this.props.sound.id } >
+        <source src={ this.props.sound.audioUrl } type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+    );
     return (
       <div id='collection-sound-player' className='collection-sound-player'>
-        <audio id='sound-audio' key={ this.props.sound.id }>
-          <source src={ this.props.sound.audioUrl } type="audio/mpeg" />
-          Your browser does not support the audio element.
-        </audio>
+        { soundAudio }
         <button id='collection-play-button' className='collection-paused' onClick={ this.playAudio }></button>
         <div>
           <p>{ this.props.sound.title }</p>
+          <p>{ this.state.audioCurrentTime }</p>
+          <p>{ this.state.audioDuration }</p>
         </div>
       </div>
     );
