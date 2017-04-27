2c8f38a0de7e32d0251d97f9d3d513de6a93904d
Dan Choi <ygdanchoi@gmail.com>
2017-04-27 14:07:00 -0400

    Style SoundListItem

--- frontend/components/collection/sound_list_item.jsx
+++ frontend/components/collection/sound_list_item.jsx
@@ -5,8 +5,9 @@ const SoundListItem = (props) => {
   if (props.sound) {
     return (
       <li className='sound-list-item'>
-        <p className='sound-list-item-title'>{ `${parseInt(props.idx) + 1}. ${props.sound.title}` }</p>
-        <button onClick={ props.setPlayingSound(props.sound) }>play</button>
+        <button id='sound-list-item-play-button' className='sound-list-item-paused' onClick={ props.setPlayingSound(props.sound) } />
+          <p className='sound-list-item-idx'>{ `${parseInt(props.idx) + 1}.` }</p>
+          <p className='sound-list-item-title'>{ props.sound.title }</p>
       </li>
     );
   } else {
