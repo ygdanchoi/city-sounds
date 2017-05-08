91aafbd5440244b308ac60b091e015e9cb7b633f
Dan Choi <ygdanchoi@gmail.com>
2017-05-08 16:08:04 -0400

    Take store off window

--- frontend/city_sounds.jsx
+++ frontend/city_sounds.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import { fetchAllCollections, fetchUserCollections, fetchCollection } from './actions/collection_actions';
-import { fetchAllSounds, fetchCollectionSounds, fetchSound } from './actions/sound_actions';
-
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
@@ -17,12 +14,4 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store } />, root);
-
-  window.store = store;
-  window.fetchAllCollections = fetchAllCollections;
-  window.fetchUserCollections = fetchUserCollections;
-  window.fetchCollection = fetchCollection;
-  window.fetchAllSounds = fetchAllSounds;
-  window.fetchCollectionSounds = fetchCollectionSounds;
-  window.fetchSound = fetchSound;
 });
