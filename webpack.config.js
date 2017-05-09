948f12c1cfa73ac4f5f187a17a77bf52acf30396
Dan Choi <ygdanchoi@gmail.com>
2017-05-08 21:41:50 -0400

    Apply Jeff Brubaker's minified code fix

--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,13 @@ module.exports = {
   devtool: 'source-maps',
   resolve: {
     extensions: [".js", ".jsx", "*"]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
 };
